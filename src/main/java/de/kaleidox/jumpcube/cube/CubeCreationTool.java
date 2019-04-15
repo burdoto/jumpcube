6d202db964f56be8f208f036e749ae3020d83ea7
Tobias Burdow [Kaleidox] <42473485+burdoto@users.noreply.github.com>
2019-04-15 15:45:45 +0200

    Added cube logic

--- src/main/java/de/kaleidox/jumpcube/cube/CubeCreationTool.java
+++ src/main/java/de/kaleidox/jumpcube/cube/CubeCreationTool.java
@@ -1,30 +1,31 @@
 package de.kaleidox.jumpcube.cube;
 
 import de.kaleidox.jumpcube.JumpCube;
+import de.kaleidox.jumpcube.exception.InvalidArgumentCountException;
 import de.kaleidox.jumpcube.util.BukkitUtil;
 
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.util.Vector;
 
 import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
-import static de.kaleidox.jumpcube.chat.Chat.argError;
 import static de.kaleidox.jumpcube.chat.Chat.message;
 import static de.kaleidox.jumpcube.chat.MessageLevel.ERROR;
 import static de.kaleidox.jumpcube.chat.MessageLevel.INFO;
 
 public class CubeCreationTool implements Cube {
     public final Player player;
+    private final World world;
     private String name;
-    private Location[] pos = new Location[2];
+    private int[][] pos = new int[2][3];
     private BlockBar bar;
 
     public CubeCreationTool(Player player) {
         this.player = player;
+        this.world = player.getWorld();
     }
 
     public boolean isReady() {
@@ -39,10 +40,7 @@ public class CubeCreationTool implements Cube {
     }
 
     public void setPos(int y, Location location) {
-        pos[y - 1] = location;
-    }
-
-    public void createBar(Location location, Vector vector) {
+        pos[y - 1] = BukkitUtil.getXYZ(location);
     }
 
     @Override
@@ -50,14 +48,50 @@ public class CubeCreationTool implements Cube {
         return name;
     }
 
+    @SuppressWarnings("ConstantConditions")
     @Override
     public void delete() {
-
+        // release pointers
+        name = null;
+        pos = null;
+        bar = null;
     }
 
     @Override
-    public Location[] getPositions() {
+    public int[][] getPositions() {
         return pos;
+    }
+
+    public ExistingCube create() {
+        assert JumpCube.getInstance() != null;
+
+        final String basePath = "cubes." + name + ".";
+        final FileConfiguration config = JumpCube.getInstance().getConfig();
+
+        // save world
+        config.set(basePath + "world", world.getName());
+
+        // save first position
+        config.set(basePath + "pos1.x", pos[0][0]);
+        config.set(basePath + "pos1.y", pos[0][1]);
+        config.set(basePath + "pos1.z", pos[0][2]);
+
+        // save second position
+        config.set(basePath + "pos2.x", pos[1][0]);
+        config.set(basePath + "pos2.y", pos[1][1]);
+        config.set(basePath + "pos2.z", pos[1][2]);
+
+        // save bar
+        bar.save(config, basePath + "bar.");
+
+        // add name to list
+        config.set("cubes.created", (config.isSet("cubes.created")
+                ? (config.getString("cubes.created") + ";")
+                : "") + name);
+
+        JumpCube.getInstance().saveConfig();
+
+        return ExistingCube.load(config, name, bar);
     }
 
     public static final class Commands {
@@ -65,10 +99,8 @@ public class CubeCreationTool implements Cube {
             if (!validateEditability(sender, sel)) return;
 
             if ((args.length != 1 && subCommand.equals("pos"))
-                    || (args.length > 0 && !subCommand.equals("pos"))) {
-                argError(sender, args.length, subCommand.equals("pos") ? 1 : 0);
-                return;
-            }
+                    || (args.length > 0 && !subCommand.equals("pos")))
+                throw new InvalidArgumentCountException(subCommand.equals("pos") ? 1 : 0, args.length);
             if (subCommand.equals("pos") && !args[0].matches("[12]")) {
                 message(sender, ERROR, "Illegal argument: " + args[0]);
                 return;
@@ -91,9 +123,9 @@ public class CubeCreationTool implements Cube {
                     break;
             }
 
-            Location[] pos = sel.getPositions();
+            int[][] pos = sel.getPositions();
             if (pos[0] != null && pos[1] != null) {
-                double dist = sqrt(pow(pos[1].getBlockX() - pos[0].getBlockX(), 2) + pow(pos[1].getBlockZ() - pos[0].getBlockZ(), 2));
+                double dist = sqrt(pow(pos[1][0] - pos[0][0], 2) + pow(pos[1][2] - pos[0][2], 2));
                 if (dist < 0) dist = dist * -1;
                 message(sender, INFO, "Size: " + (int) dist + " (Even size is recommended)");
             }
@@ -103,9 +135,23 @@ public class CubeCreationTool implements Cube {
             if (!validateEditability(sender, sel)) return;
 
             Player player = BukkitUtil.getPlayer(sender);
-            ((CubeCreationTool) sel).bar = new BlockBar(player, null);
+            ((CubeCreationTool) sel).bar = new BlockBar(player);
 
             message(sender, INFO, "The BlockBar has been pasted relative to you.");
+        }
+
+        public static void confirm(CommandSender sender, Cube sel) {
+            if (!validateEditability(sender, sel)) return;
+
+            if (!((CubeCreationTool) sel).isReady()) {
+                message(sender, ERROR, "Cube setup isn't complete yet!");
+                return;
+            }
+
+            ExistingCube cube = ((CubeCreationTool) sel).create();
+            cube.generate();
+
+            message(sender, INFO, "Cube " + sel.getCubeName() + " was created!");
         }
 
         private static boolean validateEditability(CommandSender sender, Cube sel) {
--- src/main/java/de/kaleidox/jumpcube/cube/ExistingCube.java
+++ src/main/java/de/kaleidox/jumpcube/cube/ExistingCube.java
@@ -1,35 +1,159 @@
 package de.kaleidox.jumpcube.cube;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.bukkit.Location;
+import de.kaleidox.jumpcube.JumpCube;
+import de.kaleidox.jumpcube.exception.DuplicateCubeException;
+import de.kaleidox.jumpcube.world.Generatable;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.jetbrains.annotations.Nullable;
 
-public class ExistingCube implements Cube {
+import static de.kaleidox.jumpcube.chat.Chat.message;
+import static de.kaleidox.jumpcube.chat.MessageLevel.ERROR;
+import static de.kaleidox.jumpcube.chat.MessageLevel.INFO;
+import static de.kaleidox.jumpcube.cube.BlockBar.MaterialGroup.WALLS;
+
+public class ExistingCube implements Cube, Generatable {
     private final static Map<String, Cube> instances = new ConcurrentHashMap<>();
+    private final String name;
+    private final World world;
+    private final int[][] pos;
+    private final BlockBar bar;
+
+    private ExistingCube(String name, World world, int[][] positions, BlockBar bar) {
+        this.name = name;
+        this.world = world;
+        this.pos = positions;
+        this.bar = bar;
+
+        if (instances.containsKey(name)) throw new DuplicateCubeException(name);
+        instances.put(name, this);
+    }
 
     @Override
     public String getCubeName() {
-        return null;
+        return name;
     }
 
     @Override
     public void delete() {
+        assert JumpCube.getInstance() != null;
 
+        // remove from maps
+        instances.remove(name, this);
+        JumpCube.getInstance().selections.forEach((key, value) -> {
+            if (value == this)
+                JumpCube.getInstance().selections.remove(key, value);
+        });
     }
 
     @Override
-    public Location[] getPositions() {
-        return null;
+    public int[][] getPositions() {
+        return pos;
+    }
+
+    @Override
+    public void generate() {
+        bar.validate();
+
+        // generate outter walls
+        int higherY = (pos[0][1] < pos[1][1] ? pos[1][1] : pos[0][1]);
+
+        boolean alongZ = pos[0][0] < pos[1][0];
+        int offsetX = alongZ ? 1 : -1;
+        for (int movX = pos[0][0]; (alongZ ? movX < pos[1][0] + offsetX : movX > pos[1][0] + offsetX); movX += offsetX)
+            for (int movY = higherY; movY > 0; movY--) {
+                world.getBlockAt(movX, movY, pos[0][2]).setType(bar.getRandomMaterial(WALLS));
+                //System.out.println("CYCLE 1: movX = " + movX + " && movY = " + movY);
+            }
+        for (int movX = pos[1][0]; (alongZ ? movX > pos[0][0] - offsetX : movX < pos[0][0] - offsetX); movX -= offsetX)
+            for (int movY = higherY; movY > 0; movY--) {
+                world.getBlockAt(movX, movY, pos[1][2]).setType(bar.getRandomMaterial(WALLS));
+                //System.out.println("CYCLE 2: movX = " + movX + " && movY = " + movY);
+            }
+
+        boolean alongX = pos[0][2] < pos[1][2];
+        int offsetZ = alongX ? 1 : -1;
+        for (int movZ = pos[0][2] + offsetZ; (alongX ? movZ < pos[1][2] : movZ > pos[1][2]); movZ += offsetZ)
+            for (int movY = higherY; movY > 0; movY--) {
+                world.getBlockAt(pos[0][0], movY, movZ).setType(bar.getRandomMaterial(WALLS));
+                //System.out.println("CYCLE 3: movZ = " + movZ + " && movY = " + movY);
+            }
+        for (int movZ = pos[1][2] - offsetZ; (alongX ? movZ > pos[0][2] : movZ < pos[0][2]); movZ -= offsetZ)
+            for (int movY = higherY; movY > 0; movY--) {
+                world.getBlockAt(pos[1][0], movY, movZ).setType(bar.getRandomMaterial(WALLS));
+                //System.out.println("CYCLE 4: movZ = " + movZ + " && movY = " + movY);
+            }
+
+        // clear inner area
+        for (int movX = pos[0][0] + offsetX; (alongZ ? movX < pos[1][0] : movX > pos[1][0]); movX += offsetX)
+            for (int movZ = pos[0][2] + offsetZ; (alongX ? movZ < pos[1][2] : movZ > pos[1][2]); movZ += offsetX)
+                for (int movY = higherY; movY > 0; movY--) {
+                    world.getBlockAt(movX, movY, movZ).setType(Material.AIR);
+                    //System.out.println(String.format("CYCLE 5: movX = %d && movY = %d && movZ = %d", movX, movY, movZ));
+                }
     }
 
     @Nullable
     public static ExistingCube get(String name) {
-        return null;
+        return (ExistingCube) instances.get(name);
     }
 
     public static boolean exists(String name) {
         return instances.containsKey(name);
+    }
+
+    public static ExistingCube load(final FileConfiguration config, String name, @Nullable BlockBar bar)
+            throws DuplicateCubeException {
+        final String basePath = "cubes." + name + ".";
+
+        if (bar == null) bar = BlockBar.create(config, basePath + "bar.");
+
+        // get world
+        World world = Bukkit.getWorld(Objects.requireNonNull(config.getString(basePath + "world"),
+                "No world defined for cube: " + name));
+
+        // get positions
+        int[][] locs = new int[2][3];
+
+        locs[0][0] = config.getInt(basePath + "pos1.x");
+        locs[0][1] = config.getInt(basePath + "pos1.y");
+        locs[0][2] = config.getInt(basePath + "pos1.z");
+
+        locs[1][0] = config.getInt(basePath + "pos2.x");
+        locs[1][1] = config.getInt(basePath + "pos2.y");
+        locs[1][2] = config.getInt(basePath + "pos2.z");
+
+        assert world != null : "Unknown world: " + config.getString(basePath + "world");
+
+        return new ExistingCube(name, world, locs, bar);
+    }
+
+    public final static class Commands {
+        public static void regenerate(CommandSender sender, Cube sel) {
+            if (!validateSelection(sender, sel)) return;
+
+            ((ExistingCube) sel).generate();
+            message(sender, INFO, "Cube was regenerated!");
+        }
+
+        private static boolean validateSelection(CommandSender sender, Cube sel) {
+            if (sel == null) {
+                message(sender, ERROR, "No cube selected!");
+                return false;
+            }
+            if (!(sel instanceof ExistingCube)) {
+                message(sender, ERROR, "Cube " + sel.getCubeName() + " is not finished!");
+                return false;
+            }
+            return true;
+        }
     }
 }
