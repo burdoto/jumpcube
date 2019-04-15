1634f629e8091f481ee846b8dba32ba2a08d4a34
Tobias Burdow [Kaleidox] <42473485+burdoto@users.noreply.github.com>
2019-04-15 15:45:31 +0200

    Changed Cube interface design

--- src/main/java/de/kaleidox/jumpcube/cube/Cube.java
+++ src/main/java/de/kaleidox/jumpcube/cube/Cube.java
@@ -1,11 +1,9 @@
 package de.kaleidox.jumpcube.cube;
 
-import org.bukkit.Location;
-
 public interface Cube {
     String getCubeName();
 
     void delete();
 
-    Location[] getPositions();
+    int[][] getPositions();
 }
